f9e6dcd8cff9bd1cdb7792cf7c7beaa43872e7b6
use-lie-die <tencent.lj@gmail.com>
2024-04-14 18:01:33 +0300

    fix lint

--- src/application.js
+++ src/application.js
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default () => {
-    const form = document.querySelector('.form-container');
-    const newForm = 
-    `<form id="registrationForm">
+  const form = document.querySelector('.form-container');
+  const newForm = `<form id="registrationForm">
         <div class="form-group">
             <label for="inputName">Name</label>
             <input type="text" class="form-control" id="inputName" placeholder="Введите ваше имя" name="name" required>
@@ -15,16 +14,16 @@ export default () => {
         <input type="submit" value="Submit" class="btn btn-primary">
     </form>
     `;
-    form.innerHTML = newForm;
+  form.innerHTML = newForm;
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        axios.post('/users', {})
-            .then((response) => {
-                document.body.innerHTML = `<p>${response.data.message}</p>`;
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-    })
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    axios.post('/users', {})
+      .then((response) => {
+        document.body.innerHTML = `<p>${response.data.message}</p>`;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 };
